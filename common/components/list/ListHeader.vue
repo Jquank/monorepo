aeffbe3426cb391368ddb31e10b97f6d745dd6e5
gongjian <136412110@qq.com>
2024-04-10 15:31:25 +0800

    test premit

---
+++ common/components/list/ListHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="list-header">
+    <span class="list-header-title">{{ props.title }}</span>
+
+    <div class="right">
+      <el-button
+        v-if="showAddBtn"
+        type="primary"
+        plain
+        @click="emit('headerAdd')"
+      >
+        <el-icon class="mr4"> <Plus /> </el-icon>新增
+      </el-button>
+      <el-button
+        v-if="showDeleteBtn"
+        :disabled="props.delBtnDisabled"
+        @click="emit('headerDelete')"
+        type="danger"
+        plain
+        ><el-icon><Delete /></el-icon>删除</el-button
+      >
+      <slot name="extraBtns"></slot>
+    </div>
+  </header>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  showAddBtn: {
+    type: Boolean,
+    default: true
+  },
+  showDeleteBtn: {
+    type: Boolean,
+    default: true
+  },
+  delBtnDisabled: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const emit = defineEmits(['headerAdd', 'headerDelete']);
+</script>
+
+<style lang="less" scoped>
+.list-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 10px;
+  .list-header-title {
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 500;
+  }
+}
+</style>
---
+++ common/components/list/ListPagination.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-pagination
+    v-model:current-page="page.pageNum"
+    v-model:page-size="page.pageSize"
+    :page-sizes="[10, 20, 30, 40, 50, 100]"
+    :layout="layout"
+    :total="props.total"
+    @size-change="sizeChange"
+    @current-change="currentChange"
+  />
+</template>
+
+<script setup>
+import {
+  ref,
+  reactive,
+  useAttrs,
+  watch,
+  watchEffect,
+  onMounted,
+  onUnmounted
+} from 'vue';
+import { debounce } from 'lodash-es';
+
+const props = defineProps({
+  callback: {
+    type: Function,
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
+  }
+});
+const attrs = useAttrs();
+const page = reactive({
+  pageNum: 1,
+  pageSize: 10
+});
+const total = ref(0);
+const layoutPreset = 'total, sizes, prev, pager, next, jumper';
+let layout = ref(layoutPreset);
+
+watchEffect(() => {
+  let pageSizes = attrs['page-sizes'];
+  if (pageSizes) {
+    page.pageSize = pageSizes[0];
+  }
+  layout.value = attrs.layout || layoutPreset;
+});
+const sizeChange = () => {
+  page.pageNum = 1;
+  props.callback(page);
+};
+const currentChange = () => {
+  props.callback(page);
+};
+props.callback(page);
+const initLayout = debounce(() => {
+  if (document.documentElement.clientWidth < 1050) {
+    layout.value = layout.value
+      .split(',')
+      .filter(str => !['total', 'sizes', 'jumper'].includes(str.trim()))
+      .join(',');
+  } else {
+    layout.value = layoutPreset;
+  }
+}, 100);
+onMounted(() => {
+  initLayout();
+  window.addEventListener('resize', initLayout);
+});
+onUnmounted(() => {
+  window.removeEventListener('resize', initLayout);
+});
+</script>
+
+<style lang="less" scoped>
+.el-pagination {
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px 10px 0;
+}
+</style>
